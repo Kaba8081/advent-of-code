1ad7748558544499a05defb7f0dd969af57e1036
Kaba <kaba8081@gmail.com>
2023-12-03 15:25:50 +0100

    day 3 solutions rewrite

--- 2023/3/main.py
+++ 2023/3/main.py
@@ -7,6 +7,7 @@ from string import punctuation
 global DIR_PATH
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
+# universal functions
 def get_input():
     input = None
     if os.path.isfile(os.path.join(DIR_PATH, "input.txt")):
@@ -17,176 +18,129 @@ def get_input():
         print("Error! Input file does not exist!")
         sys.exit()
 
-def check_left(line, y):
+def check_side(line, index, step):
     number = ""
     numeric = [str(i) for i in range(10)]
 
-    for i in range(y-1, -1, -1):
-        if line[i] in numeric:
-            number = line[i] + number
-        else:
-            break
+    if step == -1: # check left
+        for i in range(index-1, -1, -1):
+            if line[i] in numeric:
+                number = line[i] + number
+            else:
+                break
+    elif step == 1: # check right
+        for i in range(index+1, len(line), 1):
+            if line[i] in numeric:
+                number += line[i]
+            else:
+                break
     
     return number
 
-def check_right(line, y):
-    number = ""
+# part 1 functions
+def sum_nearby(input, input_index, line_index):
+    part_sum = 0
     numeric = [str(i) for i in range(10)]
 
-    for i in range(y+1, len(line), 1):
-        if line[i] in numeric:
-            number += line[i]
-        else:
-            break
-
-    return number
+    # check if middle is a number
+    if input[input_index][line_index] in numeric:
+        # left + middle + right
+        number = ""
+        number += check_side(input[input_index], line_index, -1) # left side
+        number += input[input_index][line_index] 
+        number += check_side(input[input_index], line_index, 1) # right side
+        part_sum = int(number)
+    else: # check left and right
+        if input[input_index][line_index-1] in numeric: # left side is a number
+            # left + number
+            number = ""
+            number += check_side(input[input_index], line_index-1, -1) # left side
+            number += input[input_index][line_index-1]
+            part_sum += int(number)
+        if input[input_index][line_index+1] in numeric: # right side is a number
+            # number + right
+            number = ""
+            number += input[input_index][line_index+1]
+            number += check_side(input[input_index], line_index+1, 1) # right side
+            part_sum += int(number)
+    
+    return part_sum
 
 def part1(input):
     part_sum = 0
-    # last_part_sum = 0
     symbols = list(punctuation)
-    symbols.remove(".")
+    symbols.remove(".") # remove dot from symbols
+
+    for i, line in enumerate(input):
+        for j, symbol in enumerate(line):
+            if symbol in symbols: # check only symbols
+                # select line above
+                if i > 0:
+                    part_sum += sum_nearby(input, i-1, j)
+                
+                # select same line
+                part_sum += sum_nearby(input, i, j)
+                
+                # select line below
+                if i < len(input)-1:
+                    part_sum += sum_nearby(input, i+1, j)
+                    
+    return part_sum
+
+# part 2 functions
+def find_nearby(input, input_index, line_index):
+    found_nums = []
     numeric = [str(i) for i in range(10)]
 
-    for input_i, line in enumerate(input):
-        
-        for line_i, symbol in enumerate(line):
-            if symbol in symbols:
-                
-                # line above
-                if input_i > 0:
-                    if input[input_i-1][line_i] in numeric: # number directly to above the symbol -> left + number + right
-                        number = check_left(input[input_i-1], line_i)
-                        number += input[input_i-1][line_i]
-                        number += check_right(input[input_i-1], line_i)
-                        part_sum += int(number) if number else 0
-                    else:
-                        if input[input_i-1][line_i-1] in numeric: # top left corner -> left + number
-                            number = check_left(input[input_i-1], line_i-1)
-                            number += input[input_i-1][line_i-1]
-                            part_sum += int(number) if number else 0
-                        if input[input_i-1][line_i+1] in numeric: # top right corner -> number + right
-                            number = input[input_i-1][line_i+1]
-                            number += check_right(input[input_i-1], line_i+1)
-                            part_sum += int(number) if number else 0
-
-                # same line
-                if input[input_i][line_i-1] in numeric: # left + number
-                    number = check_left(line, line_i)
-                    part_sum += int(number) if number else 0
-                if input[input_i][line_i+1] in numeric: # number + right
-                    number = check_right(line, line_i)
-                    part_sum += int(number) if number else 0
-                
-                # line below
-                if input_i < len(input)-1:
-                    # directly below
-                    if input[input_i+1][line_i] in numeric: # left + number + right
-                        number = check_left(input[input_i+1], line_i)
-                        number += input[input_i+1][line_i]
-                        number += check_right(input[input_i+1], line_i)
-                        part_sum += int(number) if number else 0
-                    else:
-                        if input[input_i+1][line_i-1] in numeric: # bottom left corner -> left + number
-                            number = check_left(input[input_i+1], line_i-1)
-                            number += input[input_i+1][line_i-1]
-                            part_sum += int(number) if number else 0
-                        if input[input_i+1][line_i+1] in numeric: # bottom right corner -> number + right
-                            number = input[input_i+1][line_i+1]
-                            number += check_right(input[input_i+1], line_i+1)
-                            part_sum += int(number) if number else 0
-
-                # print(input[input_i-1] if input_i > 0 else "")
-                # print(line)
-                # print(input[input_i+1] if input_i < len(input) else "")
-                # print(part_sum - last_part_sum)
-                # print("position:", input_i, line_i)
-                # print("-"*20)
-                #last_part_sum = part_sum
-
-    return part_sum
+    # check if middle is a number
+    if input[input_index][line_index] in numeric:
+        # left + middle + right
+        number = ""
+        number += check_side(input[input_index], line_index, -1) # left side
+        number += input[input_index][line_index] 
+        number += check_side(input[input_index], line_index, 1) # right side
+        found_nums.append(int(number))
+    else: # check left and right
+        if input[input_index][line_index-1] in numeric: # left side is a number
+            # left + number
+            number = ""
+            number += check_side(input[input_index], line_index-1, -1) # left side
+            number += input[input_index][line_index-1]
+            found_nums.append(int(number))
+        if input[input_index][line_index+1] in numeric: # right side is a number
+            # number + right
+            number = ""
+            number += input[input_index][line_index+1]
+            number += check_side(input[input_index], line_index+1, 1) # right side
+            found_nums.append(int(number))
+    
+    return found_nums
 
 def part2(input):
     ratio_sum = 0
-    symbols = list(punctuation)
-    symbols.remove(".")
-    numeric = [str(i) for i in range(10)]
 
-    for input_i, line in enumerate(input):
-        
-        for line_i, symbol in enumerate(line):
-            if symbol == "*": # only check gears this time
-                num_list = []
+    for i, line in enumerate(input):
+        for j, symbol in enumerate(line):
+            nearby_nums = []
 
-                # line above
-                if input_i > 0:
-                    if input[input_i-1][line_i] in numeric: # number directly to above the symbol -> left + number + right
-                        number = check_left(input[input_i-1], line_i)
-                        number += input[input_i-1][line_i]
-                        number += check_right(input[input_i-1], line_i)
-                        num_list.append(int(number)) if number else 0
-                    else:
-                        if input[input_i-1][line_i-1] in numeric: # top left corner -> left + number
-                            number = check_left(input[input_i-1], line_i-1)
-                            number += input[input_i-1][line_i-1]
-                            num_list.append(int(number)) if number else 0
-                        if input[input_i-1][line_i+1] in numeric: # top right corner -> number + right
-                            number = input[input_i-1][line_i+1]
-                            number += check_right(input[input_i-1], line_i+1)
-                            num_list.append(int(number)) if number else 0
-
-                # same line
-                if input[input_i][line_i-1] in numeric: # left + number
-                    number = check_left(line, line_i)
-                    num_list.append(int(number)) if number else 0
-                if input[input_i][line_i+1] in numeric: # number + right
-                    number = check_right(line, line_i)
-                    num_list.append(int(number)) if number else 0
+            if symbol == "*": # check only gears
+                # select line above
+                if i > 0:
+                    nearby_nums += find_nearby(input, i-1, j)
                 
-                # line below
-                if input_i < len(input)-1:
-                    # directly below
-                    if input[input_i+1][line_i] in numeric: # left + number + right
-                        number = check_left(input[input_i+1], line_i)
-                        number += input[input_i+1][line_i]
-                        number += check_right(input[input_i+1], line_i)
-                        num_list.append(int(number)) if number else 0
-                    else:
-                        if input[input_i+1][line_i-1] in numeric: # bottom left corner -> left + number
-                            number = check_left(input[input_i+1], line_i-1)
-                            number += input[input_i+1][line_i-1]
-                            num_list.append(int(number)) if number else 0
-                        if input[input_i+1][line_i+1] in numeric: # bottom right corner -> number + right
-                            number = input[input_i+1][line_i+1]
-                            number += check_right(input[input_i+1], line_i+1)
-                            num_list.append(int(number)) if number else 0
+                # select same line
+                nearby_nums += find_nearby(input, i, j)
+                
+                # select line below
+                if i < len(input)-1:
+                    nearby_nums += find_nearby(input, i+1, j)
 
-                # print(input[input_i-1] if input_i > 0 else "")
-                # print(line)
-                # print(input[input_i+1] if input_i < len(input) else "")
-                # print(part_sum - last_part_sum)
-                # print("position:", input_i, line_i)
-                # print("-"*20)
-                #last_part_sum = part_sum
-
-                if len(num_list) == 2:
-                    ratio_sum += (num_list[0] * num_list[1])
+                ratio_sum += nearby_nums[0] * nearby_nums[1] if len(nearby_nums) == 2 else 0
 
     return ratio_sum
 
 if __name__ == "__main__":
     input = get_input()
-
-#     input = """467..114.. 
-# ...*......
-# ..35..633.
-# ......#...
-# 617*......
-# .....+.58.
-# ..592.....
-# ......755.
-# ...$.*....
-# .664.598..""".strip().splitlines() # test input
 
     if not input:
         print("Error! Input file is empty!")
