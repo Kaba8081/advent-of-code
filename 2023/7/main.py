9751a548cd0d7e95b590426cc611d83e8df3b631
Kaba <kaba8081@gmail.com>
2023-12-09 00:03:20 +0100

    Solved day 7 part 1

---
+++ 2023/7/main.py
@@ -0,0 +1,114 @@
+# 'Advent of code' solution for year 2023 day 7
+import os
+import sys
+
+from dataclasses import dataclass
+from enum import IntEnum
+                        
+global DIR_PATH
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+
+def get_input():
+    input = None
+    if os.path.isfile(os.path.join(DIR_PATH, "input.txt")):
+        with open(os.path.join(DIR_PATH, "input.txt"), "r") as file:    
+            input = file.read().strip().splitlines()
+        return input
+    else:
+        print("Error! Input file does not exist!")
+        sys.exit()
+
+class CardType(IntEnum):
+    HIGH_CARD = 0
+    ONE_PAIR = 1
+    TWO_PAIR = 2
+    THREE_OAK = 3
+    FULL_HOUSE = 4
+    FOUR_OAK = 5
+    FIVE_OAK = 6
+
+@dataclass
+class Card:
+    value: str
+    type: CardType
+    bet: int
+    rank: int
+
+    def __str__(self):
+        return f"{self.value} {self.type} {self.bet} {self.rank}"
+    
+    def rankCard(self, cards_list) -> None:
+        cards_list.remove(self) # remove self from list
+        
+        # rank = count of worse cards + 1 
+        count = 0
+        points = {}
+        values = "A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, 2".split(", ")
+        for i in range(len(values)): points[values[i]] = len(values)-i
+
+        for card in cards_list:
+            if self.type > card.type:
+                count += 1
+            elif card.type == self.type: # check every letter until one card is better
+                for index, letter in enumerate(self.value):
+                    if points[letter] > points[card.value[index]]:
+                        count += 1
+                        break
+                    elif points[letter] == points[card.value[index]]:
+                        continue
+                    else:
+                        break
+        self.rank = count + 1
+
+
+def checkCard(card) -> CardType:
+    # Five of a kind, where all five cards have the same label: AAAAA
+    # Four of a kind, where four cards have the same label and one card has a different label: AA8AA
+    # Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
+    # Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
+    # Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
+    # One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
+    # High card, where all cards' labels are distinct: 23456
+
+    symbols = list(set(card.upper()))
+
+    if len(symbols) == 1:
+        return CardType.FIVE_OAK # Five of a kind
+    elif len(symbols) == 2:
+        if card.count(symbols[0]) == 4 or card.count(symbols[1]) == 4:
+            return CardType.FOUR_OAK # Four of a kind
+        else:
+            return CardType.FULL_HOUSE # Full house
+    elif len(symbols) == 3:
+        if card.count(symbols[0]) == 3 or card.count(symbols[1]) == 3 or card.count(symbols[2]) == 3:
+            return CardType.THREE_OAK # Three of a kind
+        else:
+            return CardType.TWO_PAIR # Two pair
+    elif len(symbols) == 4:
+        return CardType.ONE_PAIR # One pair
+    else:
+        return CardType.HIGH_CARD # High card
+    
+def part1(input) -> int:
+    # check type of each card -> rank cards -> sum ranks * bet
+    cards = []
+
+    for line in input:
+        card, bet = line.split(" ")
+        type = checkCard(card)
+        cards.append(Card(card, type, int(bet), 0))
+    
+    for card in cards:
+        card.rankCard(cards.copy())
+
+    return sum(card.rank * card.bet for card in cards)
+
+if __name__ == "__main__":
+    input = get_input()
+    if not input:
+        print("Error! Input file is empty!")
+        sys.exit()
+    
+    result1 = part1(input)
+    print("[7.1] Sum of all bets x ranks: ", result1)
+    sys.exit()
