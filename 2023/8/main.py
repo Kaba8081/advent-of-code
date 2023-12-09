ab0b8e86fd5374aa339074f10ae1c9c42eaadcd4
Kaba <kaba8081@gmail.com>
2023-12-10 00:46:37 +0100

    Solved day 8 part 1

---
+++ 2023/8/main.py
@@ -0,0 +1,54 @@
+# 'Advent of code' solution for year 2023 day 8
+import os
+import sys
+                        
+global DIR_PATH
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+
+def get_input():
+    input = None
+    if os.path.isfile(os.path.join(DIR_PATH, "input.txt")):
+        with open(os.path.join(DIR_PATH, "input.txt"), "r") as file:    
+            input = file.read().strip().splitlines()
+        return input
+    else:
+        print("Error! Input file does not exist!")
+        sys.exit()
+
+def part1(input) -> int:
+    instructions = input[0]
+    current_pos = "AAA"
+    moves = 0
+    map = {}
+
+    for line in input[2:]:
+        line = line.split(" = ")
+        line[1] = line[1].split(", ")
+
+        map[line[0]] = (line[1][0][1:], line[1][1][:-1])
+    
+    while current_pos != "ZZZ":
+        moves += 1
+        if instructions[(moves - 1) % len(instructions)] == "L":
+            current_pos = map[current_pos][0]
+        else:
+            current_pos = map[current_pos][1]
+
+    return moves
+
+if __name__ == "__main__":
+    input = get_input()
+    if not input:
+        print("Error! Input file is empty!")
+        sys.exit()
+
+    input1 = """LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)""".splitlines()
+    
+    result1 = part1(input)
+    print("[8.1] How many steps are required to reach ZZZ: ", result1)
+    
+    sys.exit()
