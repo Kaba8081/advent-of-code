c0b838bf2b04025f4e9aa29b162a21bdd8012bd3
Kaba <kaba8081@gmail.com>
2023-12-22 18:18:49 +0100

    Solution: day 11 part 2

--- 2023/11/main.py
+++ 2023/11/main.py
@@ -71,6 +71,68 @@ def part1(input) -> int:
 
     return int(dist_sum/2)
 
+def findEmptySpaceBetween(empty_spaces, planet1, planet2) -> int:
+    result = 0
+
+    # find empty collumns between planets
+    for x in range(*sorted((planet1[0], planet2[0]))):
+        if x in empty_spaces[1]:
+            result += 1
+    
+    # find empty rows between planets
+    for y in range(*sorted((planet1[1], planet2[1]))):
+        if y in empty_spaces[0]:
+            result += 1
+    
+    return result
+
+def methodWithoutExpanding(input, expand_amount) -> int:
+    dist_sum = 0
+
+    galaxy = [list(row) for row in input]
+    planets = {} # id: [x, y]
+    spaces = [[], []] # [[y1, y2, ...], [x1, x2, ...]]
+
+    for y in range(len(galaxy)):
+        for x in range(len(galaxy[y])):
+            if galaxy[y][x] == "#":
+                planets[len(planets.keys())] = [x, y]
+
+    # find empty collumns between planets
+    for x in range(0, len(galaxy)):
+        empty = True
+        for y in range(len(galaxy[x])):
+            if galaxy[y][x] != ".":
+                empty = False
+                break
+        if empty:
+            spaces[1].append(x)
+    
+    # find empty rows between planets
+    for y in range(0, len(galaxy)):
+        empty = True
+        for x in range(len(galaxy[y])):
+            if galaxy[y][x] != ".":
+                empty = False
+                break
+        if empty:
+            spaces[0].append(y)
+
+    for i in range(len(planets.keys())):
+        for j in range(len(planets.keys())):
+            if i > j: continue
+            else:
+                empty_space = findEmptySpaceBetween(spaces, planets[i], planets[j])
+                dist_sum += abs(planets[i][0] - planets[j][0]) + abs(planets[i][1] - planets[j][1]) + empty_space * expand_amount - empty_space
+
+    return dist_sum
+
+def part1WithoutExpanding(input) -> int:
+    return methodWithoutExpanding(input, 2)
+
+def part2(input) -> int:
+    return methodWithoutExpanding(input, 1000000)
+
 if __name__ == "__main__":
     input = get_input()
     if not input:
@@ -79,5 +141,9 @@ if __name__ == "__main__":
 
     result1 = part1(input)
     print("[11.1] Sum of all lenghts between planets: ", result1)
+    result1 = part1WithoutExpanding(input)
+    print("[11.1] Sum of all lenghts between planets (using a method without expanding galaxies): ", result1)
+    result2 = part2(input)
+    print("[11.2] Sum of all lenghts between planets in the much older galaxy: ", result2)
     
     sys.exit()
