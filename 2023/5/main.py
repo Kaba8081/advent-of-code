ed30221290f4b9df255d6dd371979579db8a31f1
Kaba <kaba8081@gmail.com>
2023-12-22 17:46:39 +0100

    Refactor: code cleanup

--- 2023/5/main.py
+++ 2023/5/main.py
@@ -90,40 +90,6 @@ if __name__ == "__main__":
     if not input:
         print("Error! Input file is empty!")
         sys.exit()
-    
-    input1 = """seeds: 79 14 55 13
-
-seed-to-soil map:
-50 98 2
-52 50 48
-
-soil-to-fertilizer map:
-0 15 37
-37 52 2
-39 0 15
-
-fertilizer-to-water map:
-49 53 8
-0 11 42
-42 0 7
-57 7 4
-
-water-to-light map:
-88 18 7
-18 25 70
-
-light-to-temperature map:
-45 77 23
-81 45 19
-68 64 13
-
-temperature-to-humidity map:
-0 69 1
-1 0 69
-
-humidity-to-location map:
-60 56 37
-56 93 4""".strip().split("\n\n")
 
     result1 = part1(input)
     print("[5.1] Lowest location number: ", result1)
